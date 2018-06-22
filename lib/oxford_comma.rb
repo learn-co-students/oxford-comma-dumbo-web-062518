02c76aff931caa2cebfbe903ee28385a2d919653
Jane Costa <yberdysh@gmail.com>
2018-06-22 09:00:21 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+	if array.size == 1
+		return array.join("")
+	elsif array.size == 2	
+		return array.join(" and ")
+	else 
+		final_el = array.pop
+		string = array.join(", ")
+		string += ", and #{final_el}"
+	end
 end
